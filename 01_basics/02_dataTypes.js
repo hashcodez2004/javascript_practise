694890baf47dfdc74baed0f9503e556a5ab7f7c0
hashcodez2004 <113353906+hashcodez2004@users.noreply.github.com>
2024-04-21 10:14:54 +0000

    commit

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict" /*Treats all js code as newer version. Strict mode makes it easier to write "secure" JavaScript. Strict mode changes previously accepted "bad syntax" into real errors.*/
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint => if size is larger than number
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object...........is a reason for many errors in JS.
